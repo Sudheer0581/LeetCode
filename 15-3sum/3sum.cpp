108b195a28cb3406c0dfbcead81790e7ffaabf3a
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-01-17 17:34:28 +0530

    Time: 104 ms (81.26%) | Memory: 24.4 MB (70.87%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int l,r;
-        vector<vector<int>>v1;
-        vector<int>v;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            l=i+1;
-            r=nums.size()-1;
-            while(l<r){
-                if(nums[i]+nums[l]+nums[r]==0){
-                    v.push_back(nums[i]);
-                    v.push_back(nums[l]);
-                    v.push_back(nums[r]);
-                    l+=1;
-                    r-=1;
-                    // for(int i=0;i<v1.size();i++){
-                    //     auto it = std::find(v1.begin(), v1.end(),v);
-                    //     if(it==v1.end()){
-                    //         v1.push_back(v);
-                    //     }
-                    // }
-                     v1.push_back(v);
-                    v.clear();
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0 ; i < n - 2 ; i++){
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+
+                    while(left < right && nums[left] == nums[left + 1]){
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+
+                 left++;
+                right--;
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
                 }
-                else if(nums[i]+nums[l]+nums[r]<0){
-                    l+=1;
-                }
-                else{
-                    r-=1;
-                }
+
             }
         }
-        map<vector<int>,int>mp;
-        for(int i=0;i<v1.size();i++){
-            mp[v1[i]]++;
-        }
-        v1.clear();
-        for(auto it:mp){
-            v1.push_back(it.first);
-        }
-        return v1;
+        return result;
     }
 };
