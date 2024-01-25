af964091338b2662f0ed4f691bf79e6ab5488d65
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-01-25 11:28:02 +0530

    Time: 172 ms (17.32%) | Memory: 107.6 MB (5.10%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,7 +3,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         int arr[10000001]={0};
         for(auto it:nums){
-            cout<<it<<" ";
+            // cout<<it<<" ";
             arr[it]++;
             if(arr[it]>1) return it;
         }
