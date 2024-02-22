4e347e9b66130e9c2905b8ac65246a2a47ae423f
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-02-22 10:23:27 +0530

    Time: 141 ms (56.16%) | Memory: 66 MB (48.44%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& t) {
+        if(n==1 and t.size()==0) return 1;
+        if(n>1 and t.size()==0) return -1;
+        int s=0;
+        map<int,int>mp;
+        map<int,int>mp1;
+        for(int i=0;i<t.size();i++){
+            mp[t[i][0]]++;
+            mp1[t[i][1]]++;
+        }
+        int m=0,k=0;
+        for(auto it:mp1){
+            if(it.second>m){
+                m=it.second;
+                k=it.first;
+            }
+        }
+        if(mp.size()==n-1 and mp.size()==m and mp.find(k)==mp.end()){
+            return k;
+        }
+        return -1;
+    }
+};
