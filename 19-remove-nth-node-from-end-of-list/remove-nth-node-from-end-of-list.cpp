e329cf48ad5e1cb2ce8f71dfa87db3e7abc271a9
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-03-03 20:54:21 +0530

    Time: 3 ms (64.94%) | Memory: 13.2 MB (66.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -20,9 +20,9 @@ public:
         int x=c-n+1;
         if(head==NULL) return NULL;
         if(x==1){
-            ListNode*temp=head;
+            // ListNode*temp=head;
             head= head->next;
-            delete temp;
+            // delete temp;
             return head;
         }
         ListNode* temp1=head;
@@ -30,7 +30,7 @@ public:
         for(int i=0;i<x-2;i++){
             temp1=temp1->next;
         }
-        cout<<temp1->val<<endl;
+        // cout<<temp1->val<<endl;
         temp1->next=temp1->next->next;
         return head;
 
