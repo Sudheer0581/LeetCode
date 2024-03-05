eea4cbbae6a36c16680409d948d954da86169ccf
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-03-05 11:18:52 +0530

    Time: 30 ms (22.76%) | Memory: 14 MB (13.45%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        if(s.size()==1){
-            return 1;
-        }
-        else{
             int i=0,j=s.size()-1;
             while(i<j and s[i]==s[j]){
                 int z=s[i];
                 while(i<=j and s[i]==z){
                     i+=1;
                 }
-                while(i<=j and s[j]==z){
+                while(j>=i and s[j]==z){
                     j-=1;
                 }
             }
-            cout<<i<<" "<<j;
-            if(i<=j){
-                return j-i+1;
-            }
-            return 0;
+            return j-i+1;
         }
-    }
 };
