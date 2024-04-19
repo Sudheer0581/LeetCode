d761e38a561d2ab9ea6e04fe6ec2b39894034940
Sudheer <96948623+Sudheer0581@users.noreply.github.com>
2024-04-19 21:59:15 +0530

    Time: 4 ms (61.90%) | Memory: 28.4 MB (6.93%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    long long int dp[1001];
+    int fun(vector<int>piles,int i){
+        if(i>=piles.size()){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int pick = piles[i]+fun(piles,i+1);
+        int nonpick = piles[piles.size()-i-1]+fun(piles,i+1);
+        return dp[i] = max(pick,nonpick);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp,-1,sizeof(dp));
+        int x = fun(piles,0);
+        int y = fun(piles,1);
+        if(x>y) return true;
+        return false;
+
     }
 };
